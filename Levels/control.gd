9ba341371a0e6445110a716f72bc676da194eb44
Jessica Baum <110245648+jnbaum@users.noreply.github.com>
2025-04-15 11:46:12 -0500

    Rearranged opening scenes to help with music continuation. Added the beginnings of an explainer page.

--- Levels/control.gd
+++ Levels/control.gd
@@ -3,6 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$FirstOpen.visible = true
+	$Story.visible = false
+	$Explainer.visible = false
 	pass # Replace with function body.
 
 
@@ -17,7 +20,7 @@ func _process(delta: float) -> void:
 func _on_settings_button_pressed() -> void:
 	$ClickSound.play()
 	await get_tree().create_timer(1.25).timeout
-	get_tree().change_scene_to_file("res://settings.tscn")
+	#get_tree().change_scene_to_file("res://settings.tscn")
 	pass # Replace with function body.
 	
 
@@ -26,7 +29,9 @@ func _on_settings_button_pressed() -> void:
 func _on_play_button_pressed() -> void:
 	$ClickSound.play()
 	await get_tree().create_timer(1.25).timeout
-	get_tree().change_scene_to_file("res://FirstCut.tscn")
+	$FirstOpen.visible = false
+	$Story.visible = true
+	#get_tree().change_scene_to_file("res://FirstCut.tscn")
 
 
 
@@ -43,4 +48,25 @@ func _on_load_button_pressed() -> void:
 	$ClickSound.play()
 	await get_tree().create_timer(1.25).timeout
 	#PUT STUFF HERE
+	pass # Replace with function body.
+	
+	
+	
+
+
+func _on_play_2_pressed() -> void:
+	$ClickSound.play()
+	await get_tree().create_timer(1.25).timeout
+	$FirstOpen.visible = false
+	$Story.visible = false
+	get_tree().change_scene_to_file("res://Levels/level.tscn")
+	pass # Replace with function body.
+
+
+func _on_next_pressed() -> void:
+	$ClickSound.play()
+	await get_tree().create_timer(1.25).timeout
+	$FirstOpen.visible = false
+	$Story.visible = false
+	$Explainer.visible = true
 	pass # Replace with function body.
